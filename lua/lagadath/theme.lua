99fefcc0b64ea184797d53088c4cd9143b4dc1ca
Dimitar Haralanov <dharalanov@me.com>
2025-06-25 16:13:41 +0200

    Errr, change the color to a better one

--- lua/lagadath/theme.lua
+++ lua/lagadath/theme.lua
@@ -183,7 +183,7 @@ function M.get()
     ['@variable.builtin'] = { link = 'Special' },
     ['@variable.parameter'] = { link = 'Identifier' },
     ['@variable.parameter.builtin'] = { link = 'Identifier' },
-    ['@variable.member'] = { fg = blend(p.iris, p.base, 0.6) },
+    ['@variable.member'] = { fg = blend(p.text, p.base, 0.7) },
 
     ['@constant'] = { link = 'Constant' },
     ['@constant.builtin'] = { link = 'Special' },
